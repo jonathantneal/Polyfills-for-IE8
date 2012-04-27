0f5b840085252e431d3a1623f5a619f080ef6b3e
Jonathan Neal <jonathantneal@hotmail.com>
2012-04-27 11:27:07 -0700

    Shorthand properties

--- getComputedStyle.js
+++ getComputedStyle.js
@@ -1,5 +1,5 @@
 // getComputedStyle
-!('getComputedStyle' in this) && this.getComputedStyle = (function () {
+!('getComputedStyle' in this) && (this.getComputedStyle = (function () {
 	function getPixelSize(element, sizeWithSuffix, rootSize) {
 		var
 		size = parseFloat(sizeWithSuffix),
@@ -12,24 +12,42 @@
 		return (suffix == 'em') ? size * rootSize : (suffix == 'in') ? size * 96 : (suffix == 'pt') ? size * 96 / 72 : (suffix == '%') ? size / 100 * rootSize : size;
 	}
 
+	function setShortStyleProperty(style, property) {
+		var
+		borderSuffix = property == 'border' ? 'Width' : '',
+		t = property + 'Top' + borderSuffix,
+		r = property + 'Right' + borderSuffix,
+		b = property + 'Bottom' + borderSuffix,
+		l = property + 'Left' + borderSuffix;
+
+		style[property] = (style[t] == style[r] == style[b] == style[l] ? [style[t]]
+		: style[t] == style[b] && style[l] == style[r] ? [style[t], style[r]]
+		: style[l] == style[r] ? [style[t], style[r], style[b]]
+		: [style[t], style[r], style[b], style[l]]).join(' ');
+	}
+
 	function getComputedStyle(element) {
 		var
 		currentStyle = element.currentStyle,
-		styles = {},
+		style = {},
 		fontSize = getPixelSize(element, currentStyle.fontSize, null);
 
 		for (property in currentStyle) {
+			style[property] = currentStyle[property];
+
 			if (/(margin|padding)(Top|Right|Bottom|Left)|border(Top|Right|Bottom|Left)Width/.test(property)) {
-				styles[property] = getPixelSize(element, currentStyle[property], fontSize) + 'px';
-			} else {
-				styles[property] = currentStyle[property];
+				style[property] = getPixelSize(element, style[property], fontSize) + 'px';
 			}
 		}
 
-		styles.fontSize = fontSize + 'px';
+		setShortStyleProperty(style, 'margin');
+		setShortStyleProperty(style, 'padding');
+		setShortStyleProperty(style, 'border');
 
-		return styles;
+		style.fontSize = fontSize + 'px';
+
+		return style;
 	}
 
 	return getComputedStyle;
-})(this);
+})(this));
